5bb8ee92d08fca94b695f6ee1420d7b836d72717
HelloMandu <tjdals6695@gmail.com>
2020-08-25 17:56:59 +0900

    ReactStudy

--- news-viewer/src/pages/NewsPage.js
+++ news-viewer/src/pages/NewsPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import NewsList from '../components/NewsList';
 import Categories from '../components/Categories';
 
